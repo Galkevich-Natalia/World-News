736d8f7b5a9f6d57a4daec896f9e6f5bf94d5bf0
Galkevich-Natalia <galkevich.natalia@gmail.com>
2023-10-25 02:23:31 +0300

    fixed style of links in header in news categories

--- src/components/general/components/header/components/navMenu/navMenu.tsx
+++ src/components/general/components/header/components/navMenu/navMenu.tsx
@@ -6,49 +6,49 @@ export const NavMenu = () => {
         <ItemContainer>
             <NavLink
                 to="/"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-home")}
+                className={({ isActive }) => (isActive ? "active-link-class" : "navLink link-home")}
             >
                 Home
             </NavLink>
             <NavLink
                 to="/category/Society"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-society")}
+                className={({ isActive }) => (isActive ? " active-link-class" : "navLink link-society")}
             >
                 Society
             </NavLink>
             <NavLink
                 to="/category/Business"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-business")}
+                className={({ isActive }) => (isActive ? "active-link-class" : "navLink link-business")}
             >
                 Business
             </NavLink>
             <NavLink
                 to="/category/Science"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-science")}
+                className={({ isActive }) => (isActive ? "active-link-class" : "navLink link-science")}
             >
                 Science
             </NavLink>
             <NavLink
                 to="/category/Arts"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-arts")}
+                className={({ isActive }) => (isActive ? " active-link-class" : "navLink link-arts")}
             >
                 Arts
             </NavLink>
             <NavLink
                 to="/category/Sports"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-sports")}
+                className={({ isActive }) => (isActive ? "active-link-class" : "navLink link-sports")}
             >
                 Sports
             </NavLink>
             <NavLink
                 to="/category/Health"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-health")}
+                className={({ isActive }) => (isActive ? " active-link-class" : "navLink link-health")}
             >
                 Health
             </NavLink>
             <NavLink
                 to="/category/Games"
-                className={({ isActive }) => (isActive ? "navLink active-link-class" : "navLink link-games")}
+                className={({ isActive }) => (isActive ? "active-link-class" : "navLink link-games")}
             >
                 Games
             </NavLink>
